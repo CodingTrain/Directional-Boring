81f03f04512d351960e5ed0b2215aee5a1a0b412
Daniel Shiffman <daniel@shiffman.net>
2022-06-30 20:27:41 -0400

    comment the code so far

--- sketch.js
+++ sketch.js
@@ -1,20 +1,25 @@
-let pos;
-let dir;
-let path;
+// Vectors for current position and direction
+let pos, dir;
+// Bias of current drill (up or down, 1 or -1)
 let bias;
+// All the points along the drill path so far
+let path;
+// Current state of game
 let state;
 
-// A measure of stability of ground, how much randomness is added
-let randomFactor = 5;
-
+// Groundcolor is used to determine win or lose state
 const groundColor = [139, 69, 19];
 const groundLevel = 100;
+// Position of the goal square box (relative to ground)
 const goal = { x: 540, w: 20 };
 
+// Pixel map for scene
 let hddScene;
 
+// Button to start
 let startButton;
 
+// Reset the initial state
 function startDrill() {
   pos = createVector(10, 100);
   dir = p5.Vector.fromAngle(PI / 6);
@@ -24,9 +29,11 @@ function startDrill() {
 }
 
 function setup() {
+  // Let's begin!
   createCanvas(600, 400);
   startDrill();
 
+  // Handle the start and stop button
   startButton = createButton('start').mousePressed(function () {
     if (state == 'PAUSED') {
       state = 'DRILLING';
@@ -39,13 +46,16 @@ function setup() {
     }
   });
 
+  // Handle the toggle bias button
   createButton('toggle bias').mousePressed(function () {
     bias *= -1;
   });
 
+  // A slider for adding some randomness
   createSpan('randomness: ');
   randomSlider = createSlider(0, 10, 0, 0.01);
 
+  // Draw the scene
   hddScene = createGraphics(width, height);
   hddScene.background(51);
   hddScene.noStroke();
@@ -55,12 +65,14 @@ function setup() {
   hddScene.fill(30, 144, 255);
   hddScene.arc(width / 2, groundLevel, 400, 200, 0, PI);
 
-  // Goal
+  // Add the goal
   hddScene.fill(0, 255, 0);
   hddScene.rect(goal.x, groundLevel - goal.w, goal.w, goal.w);
 }
 
+// One drill step
 function drill() {
+  // Angle the drill turns per step
   const angle = 0.01;
   dir.rotate(angle * bias);
 
@@ -69,13 +81,18 @@ function drill() {
   const r = random(-randomFactor, randomFactor) * angle;
   dir.rotate(r);
 
+  // Save previous position
   path.push(pos.copy());
   pos.add(dir);
 
+  // Get pixel color under drill
   const c = hddScene.get(pos.x, pos.y);
+
+  // Green you win!
   if (c[0] == 0 && c[1] == 255 && c[2] == 0) {
     state = 'WIN';
     startButton.html('try again');
+    // Anything else not the ground color you lose!
   } else if (
     c[0] != groundColor[0] ||
     c[1] !== groundColor[1] ||
@@ -86,11 +103,15 @@ function drill() {
   }
 }
 
+// Draw loop
 function draw() {
+  // Dril!
   if (state == 'DRILLING') drill();
 
+  // Draw the scene
   image(hddScene, 0, 0);
 
+  // Draw the path
   beginShape();
   noFill();
   stroke(0);
@@ -106,6 +127,7 @@ function draw() {
   strokeWeight(1);
   circle(10, groundLevel, 4);
 
+  // Draw the drill bit
   stroke(255, 0, 0);
   strokeWeight(2);
   push();
@@ -114,6 +136,7 @@ function draw() {
   line(0, 0, 10, 0);
   pop();
 
+  // If you've lost!
   if (state == 'LOSE') {
     background(255, 0, 0, 150);
     textAlign(CENTER, CENTER);
@@ -122,6 +145,7 @@ function draw() {
     textSize(96);
     textFont('courier-bold');
     text('YOU LOSE', width / 2, height / 2);
+    // If you've won!
   } else if (state == 'WIN') {
     background(0, 255, 0, 150);
     textAlign(CENTER, CENTER);
@@ -130,8 +154,8 @@ function draw() {
     textSize(96);
     textFont('courier-bold');
     text('YOU WIN', width / 2, height / 2);
-
     textSize(24);
+    // Starting idea for a score
     text(`pipe length: ${path.length}`, width / 2, height / 2 + 96);
   }
 }
